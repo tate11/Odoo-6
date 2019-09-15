2640b93bd8511c9f714106ce6ba60be9e2de8fcf
farhanrashad <farhan@dynexcel.com>
2019-09-15 13:24:38 +0000

    update currecny conversion

--- de_pricelist_copy_items/models/.ipynb_checkpoints/models-checkpoint.py
+++ de_pricelist_copy_items/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime
-from odoo import api, fields, models, _
-from odoo.exceptions import UserError, ValidationError
+import json
+import re
+import uuid
+from functools import partial
+
+from lxml import etree
+from dateutil.relativedelta import relativedelta
+from werkzeug.urls import url_encode
+
+from odoo import api, exceptions, fields, models, _
+from odoo.tools import email_re, email_split, email_escape_char, float_is_zero, float_compare, \
+    pycompat, date_utils
+from odoo.tools.misc import formatLang
+
+from odoo.exceptions import AccessError, UserError, RedirectWarning, ValidationError, Warning
+
+from odoo.addons import decimal_precision as dp
+import logging
 
 class PriceList(models.Model):
     _inherit = 'product.pricelist'
@@ -14,25 +29,30 @@ class PriceList(models.Model):
 class PriceListItem(models.Model):
     _inherit = 'product.pricelist.item'
 
-    price_on_basepricelist = fields.Float(string="Price on base pricelist",compute="_calculate_price_on_basepricelist")
-    price_on_basepricelist1 = fields.Float(string="Price 100",default='100')
+    price_on_basepricelist = fields.Monetary(string="Price on base pricelist",compute="_calculate_price_on_basepricelist")
+    currency_id = fields.Many2one('res.currency',related='pricelist_id.currency_id',string='Currency')
+    
     
     @api.one
-    @api.depends('pricelist_id')
+    @api.depends('pricelist_id','base_pricelist_id','product_tmpl_id','min_quantity')
     def _calculate_price_on_basepricelist(self):
-        from_amount = 200
+        from_amount = 0
         date = self._context.get('date') or fields.Date.today()
         company_id = self._context.get('company_id') or self.env['res.users']._get_company().id
-        currency = self.pricelist_id.currency_id
-        if self.base_pricelist_id:
-            if self.applied_on == '1_product':
-                from_amount = self.env['product.pricelist.item'].search([('pricelist_id', '=', self.base_pricelist_id.id),('product_tmpl_id', '=', self.product_tmpl_id.id),('min_quantity', '=', self.min_quantity)],limit=1).fixed_price
-            elif self.applied_on == '0_product_variant':
-                from_amount = self.env['product.pricelist.item'].search([('pricelist_id', '=', self.base_pricelist_id.id),('product_id', '=', self.product_id.id),('min_quantity', '=', self.min_quantity)],limit=1).fixed_price
+        for rs in self:
+            from_currency_id = rs.base_pricelist_id.currency_id
+            to_currency_id = rs.base_pricelist_id.currency_id
+            
+            if rs.base_pricelist_id:
+                if rs.applied_on == 'Product':
+                    from_amount = self.env['product.pricelist.item'].search([('pricelist_id', '=', rs.base_pricelist_id.id),('product_tmpl_id', '=', rs.product_tmpl_id.id),('min_quantity', '=', rs.min_quantity)],limit=1).fixed_price
+                elif rs.applied_on == '0_product_variant':
+                    from_amount = self.env['product.pricelist.item'].search([('pricelist_id', '=', rs.base_pricelist_id.id),('product_id', '=', rs.product_id.id),('min_quantity', '=', rs.min_quantity)],limit=1).fixed_price
             else:
                 from_amount = 0
-            self.price_on_basepricelist = currency._convert(abs(from_amount),currency,company_id,date)
-            
+                #from_amount = 0
+                #rs.price_on_basepricelist = from_currency_id._convert(abs(from_amount),rs.currency_id,company_id,date)
+            rs.price_on_basepricelist = from_amount
     
     @api.multi
     def _write(self, values):
