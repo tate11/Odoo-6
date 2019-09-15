fc3bc189e8f9e047691d395fb3d95341203fae79
Farhan Rashad <farhan@dynexcel.com>
2019-09-15 23:07:47 +0500

    update modules

--- de_carousel_tiered_pricing/__manifest__.py
+++ de_carousel_tiered_pricing/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product_tiered_pricing','de_website_product_carousel'],
+    'depends': ['base','product_tiered_pricing','website_product_carousel'],
 
     # always loaded
     'data': [
