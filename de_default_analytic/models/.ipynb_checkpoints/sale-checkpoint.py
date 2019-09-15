b9ef925ebc662505637ef2b5b2189f9a5a559f0d
farhanrashad <farhan@dynexcel.com>
2019-09-15 17:17:38 +0000

    default analytic

--- de_default_analytic/models/.ipynb_checkpoints/sale-checkpoint.py
+++ de_default_analytic/models/.ipynb_checkpoints/sale-checkpoint.py
@@ -3,26 +3,22 @@
 from odoo import models, fields, api
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = 'sale.order'            
+            
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
-        res = super(SaleOrder, self)._onchange_product_id()
-        inv_type = self.invoice_id.type
         if self.product_id:
-            ana_accounts = self.product_id.product_tmpl_id._get_product_analytic_accounts()
-            self.account_analytic_id = ana_account.id
+            ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
+            self.analytic_tag_ids = ana_tags['income']
+    
+    @api.multi
+    def _write(self, values):
+        res = super(SaleOrderLine, self)._write(values)
+        if not(self.analytic_tag_ids):
+            ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
+            self.analytic_tag_ids = ana_tags['income']
         return res
-
-    @api.model
-    def create(self, vals):
-        inv_type = self.env.context.get('inv_type', 'out_invoice')
-        if vals.get('product_id') and inv_type and \
-                not vals.get('account_analytic_id'):
-            product = self.env['product.product'].browse(
-                vals.get('product_id'))
-            ana_accounts = product.product_tmpl_id.\
-                _get_product_analytic_accounts()
-            ana_account = ana_accounts[INV_TYPE_MAP[inv_type]]
-            vals['account_analytic_id'] = ana_account.id
-        return super(AccountInvoiceLine, self).create(vals)
--- de_default_analytic/models/sale.py
+++ de_default_analytic/models/sale.py
@@ -3,12 +3,22 @@
 from odoo import models, fields, api
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = 'sale.order'            
+            
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
 
     @api.onchange('product_id')
     def _onchange_product_id(self):
-        res = super(SaleOrder, self)._onchange_product_id()
         if self.product_id:
             ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
-            self.analytic_tag_ids = ana_tags.ids
+            self.analytic_tag_ids = ana_tags['income']
+    
+    @api.multi
+    def _write(self, values):
+        res = super(SaleOrderLine, self)._write(values)
+        if not(self.analytic_tag_ids):
+            ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
+            self.analytic_tag_ids = ana_tags['income']
         return res
