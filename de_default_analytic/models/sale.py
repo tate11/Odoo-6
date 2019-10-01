c2b3b552eba364dfef2d95760731ac3864e109af
farhanrashad <farhan@dynexcel.com>
2019-10-01 08:50:07 +0000

    default analytics

--- de_default_analytic/models/sale.py
+++ de_default_analytic/models/sale.py
@@ -15,10 +15,10 @@ class SaleOrderLine(models.Model):
             ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
             self.analytic_tag_ids = ana_tags['income']
     
-    @api.multi
-    def _write(self, values):
-        res = super(SaleOrderLine, self)._write(values)
-        if not(self.analytic_tag_ids):
-            ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
-            self.analytic_tag_ids = ana_tags['income']
+    #@api.multi
+    #def _write(self, values):
+        #res = super(SaleOrderLine, self)._write(values)
+        #if not(self.analytic_tag_ids):
+            #ana_tags = self.product_id.product_tmpl_id._get_product_analytic_tags()
+            #self.analytic_tag_ids = ana_tags['income']
         return res
